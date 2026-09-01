9db4bb54cdce38e9fc29ff9f2648adf35e691a06
testcasefounder <akiruzzamanjibon3547@gmail.com>
2026-09-01 10:21:00 +0600

    supabse add korsi

---
+++ src/proxy.ts
@@ -0,0 +1,41 @@
+import { createServerClient } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
+
+export async function proxy(request: NextRequest) {
+    let response = NextResponse.next({
+        request,
+    });
+
+    const supabase = createServerClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+        {
+            cookies: {
+                getAll() {
+                    return request.cookies.getAll();
+                },
+
+                setAll(cookiesToSet, headers) {
+                    cookiesToSet.forEach(({ name, value, options }) => {
+                        request.cookies.set(name, value);
+                        response.cookies.set(name, value, options);
+                    });
+
+                    Object.entries(headers).forEach(([key, value]) => {
+                        response.headers.set(key, value);
+                    });
+                },
+            },
+        }
+    );
+
+    await supabase.auth.getClaims();
+
+    return response;
+}
+
+export const config = {
+    matcher: [
+        "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    ],
+};
